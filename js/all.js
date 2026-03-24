d967df55863671a5fa071b928584248afdd5ea64
btheb1 <developerbtheb@gmail.com>
2026-03-24 11:18:14 +0500

    Update all.js

--- js/all.js
+++ js/all.js
@@ -15,9 +15,9 @@ const episodesData = {
                     duration: "23:08", 
                     description: "Первая серия",
                     qualities: {
-                        "1080p": { file: "seas01_ser01_1080p.mp4", size: "850 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_1080p" },
-                        "720p": { file: "seas01_ser01_720p.mp4", size: "450 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" },
-                        "480p": { file: "seas01_ser01_480p.mp4", size: "250 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_480p" },
+                        "1080p": { file: "seas01_ser01_1080p.mp4", size: "859 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_1080p" },
+                        "720p": { file: "seas01_ser01_720p.mp4", size: "446 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" },
+                        "480p": { file: "seas01_ser01_480p.mp4", size: "191 MB", path: "https://drive.google.com/uc?export=download&id=194BDtu4AAQ1Jd-fIklv9Zy0nR0Q-GOvA" },
                         "360p": { file: "seas01_ser01_360p.mp4", size: "101 MB", path: "https://drive.google.com/uc?export=download&id=17CetDaNyKIHyDvratPkxdgOJ44Pvh17G" },
                         "240p": { file: "seas01_ser01_240p.mp4", size: "59.3 MB", path: "https://drive.google.com/uc?export=download&id=1FOG_AuESc-KvuhU-D343fAPubdFj_aXW" }
                     }
