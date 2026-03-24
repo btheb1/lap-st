ce15087f4a18e1f244d0add885c56444efd4c745
btheb1 <developerbtheb@gmail.com>
2026-03-24 09:44:40 +0500

    Update all.js

--- js/all.js
+++ js/all.js
@@ -15,11 +15,11 @@ const episodesData = {
                     duration: "23:08", 
                     description: "Первая серия",
                     qualities: {
-                        "1080p": { file: "ser01_1080p.mp4", size: "850 MB", path: "film/seas1/ser01_1080p.mp4" },
-                        "720p": { file: "ser01_720p.mp4", size: "450 MB", path: "film/seas1/ser01_720p.mp4" },
-                        "480p": { file: "ser01_480p.mp4", size: "250 MB", path: "film/seas1/ser01_480p.mp4" },
-                        "360p": { file: "seas01_ser01_360p.mp4", size: "101 MB", path: "film/seas01/ser01/seas01_ser01_360p.mp4" },
-                        "240p": { file: "seas01_ser01_240p.mp4", size: "59.3 MB", path: "film/seas01/ser01/seas01_ser01_240p.mp4" }
+                        "1080p": { file: "ser01_1080p.mp4", size: "850 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_1080p" },
+                        "720p": { file: "ser01_720p.mp4", size: "450 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" },
+                        "480p": { file: "ser01_480p.mp4", size: "250 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_480p" },
+                        "360p": { file: "seas01_ser01_360p.mp4", size: "101 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_360p" },
+                        "240p": { file: "seas01_ser01_240p.mp4", size: "59.3 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_240p" }
                     }
                 },
                 { 
@@ -28,8 +28,8 @@ const episodesData = {
                     duration: "24:15", 
                     description: "Вторая серия",
                     qualities: {
-                        "720p": { file: "ser02_720p.mp4", size: "430 MB", path: "film/seas1/ser02_720p.mp4" },
-                        "480p": { file: "ser02_480p.mp4", size: "240 MB", path: "film/seas1/ser02_480p.mp4" }
+                        "720p": { file: "ser02_720p.mp4", size: "430 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" },
+                        "480p": { file: "ser02_480p.mp4", size: "240 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_480p" }
                     }
                 },
                 { 
@@ -38,8 +38,8 @@ const episodesData = {
                     duration: "26:45", 
                     description: "Третья серия",
                     qualities: {
-                        "1080p": { file: "ser03_1080p.mp4", size: "890 MB", path: "film/seas1/ser03_1080p.mp4" },
-                        "720p": { file: "ser03_720p.mp4", size: "470 MB", path: "film/seas1/ser03_720p.mp4" }
+                        "1080p": { file: "ser03_1080p.mp4", size: "890 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_1080p" },
+                        "720p": { file: "ser03_720p.mp4", size: "470 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" }
                     }
                 }
             ]
@@ -55,8 +55,8 @@ const episodesData = {
                     duration: "24:45", 
                     description: "Первая серия второго сезона",
                     qualities: {
-                        "480p": { file: "ser01_480p.mp4", size: "260 MB", path: "film/seas2/ser01_480p.mp4" },
-                        "360p": { file: "ser01_360p.mp4", size: "180 MB", path: "film/seas2/ser01_360p.mp4" }
+                        "480p": { file: "ser01_480p.mp4", size: "260 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_480p" },
+                        "360p": { file: "ser01_360p.mp4", size: "180 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_360p" }
                     }
                 },
                 { 
@@ -65,19 +65,17 @@ const episodesData = {
                     duration: "25:20", 
                     description: "Вторая серия",
                     qualities: {
-                        "1080p": { file: "ser02_1080p.mp4", size: "840 MB", path: "film/seas2/ser02_1080p.mp4" },
-                        "720p": { file: "ser02_720p.mp4", size: "440 MB", path: "film/seas2/ser02_720p.mp4" },
-                        "480p": { file: "ser02_480p.mp4", size: "250 MB", path: "film/seas2/ser02_480p.mp4" }
+                        "1080p": { file: "ser02_1080p.mp4", size: "840 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_1080p" },
+                        "720p": { file: "ser02_720p.mp4", size: "440 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_720p" },
+                        "480p": { file: "ser02_480p.mp4", size: "250 MB", path: "https://drive.google.com/uc?export=download&id=ТВОЙ_ID_480p" }
                     }
                 }
             ]
         }
     ],
     
-    // Порядок качеств от лучшего к худшему
     qualityOrder: ["2160p", "1080p", "720p", "480p", "360p", "240p"],
     
-    // Функция получения доступных качеств для серии
     getAvailableQualities: function(season, episode) {
         const seasonData = this.series.find(s => s.season === season);
         if (seasonData) {
@@ -89,7 +87,6 @@ const episodesData = {
         return null;
     },
     
-    // Функция получения самого высокого доступного качества
     getBestQuality: function(season, episode) {
         const qualities = this.getAvailableQualities(season, episode);
         if (!qualities) return null;
@@ -102,7 +99,6 @@ const episodesData = {
         return null;
     },
     
-    // Функция получения самого низкого доступного качества
     getWorstQuality: function(season, episode) {
         const qualities = this.getAvailableQualities(season, episode);
         if (!qualities) return null;
@@ -116,18 +112,15 @@ const episodesData = {
         return null;
     },
     
-    // Функция получения качества по умолчанию (самое высокое)
     getDefaultQuality: function(season, episode) {
         return this.getBestQuality(season, episode);
     },
     
-    // Функция получения пути к видео
     getVideoPath: function(season, episode, quality) {
         const qualities = this.getAvailableQualities(season, episode);
         if (qualities && qualities[quality]) {
             return qualities[quality].path;
         }
-        // Если запрошенное качество недоступно, берем лучшее
         const bestQuality = this.getBestQuality(season, episode);
         if (bestQuality && qualities[bestQuality]) {
             return qualities[bestQuality].path;
@@ -135,7 +128,6 @@ const episodesData = {
         return null;
     },
     
-    // Функция получения информации о качестве
     getQualityInfo: function(season, episode, quality) {
         const qualities = this.getAvailableQualities(season, episode);
         if (qualities && qualities[quality]) {
